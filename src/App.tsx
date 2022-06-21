095e1e25ea9047c14e4a1af80949690f01349d92
João Mello <joao.mellof@furg.br>
2022-06-21 08:41:28 -0300

    refactoring project and oauth begin

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,51 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { gapi } from 'gapi-script'
+import { useEffect } from 'react'
+import { GoogleLogin, GoogleLoginResponse, GoogleLoginResponseOffline, GoogleLogout } from 'react-google-login'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  useEffect(()=>{
+    function start(){
+      gapi.client.init({
+        clientId:import.meta.env.VITE_APP_GAPI_CLIENT_ID,
+        scope: ""
+      })
+
+    }
+    gapi.load('client:auth2', start)
+    // console.log(gapi.auth.getToken().access_token)
+  })
+
+  function onSuccess(r:GoogleLoginResponse | GoogleLoginResponseOffline){
+    // if(r instanceof GoogleLoginResponse)
+    // if(r.includes())
+    if('profileObj' in r) console.log(r.profileObj)
+    // console.log(r?.profileObj)
+  }
+  function onFailure(){
+
+  }
+
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <GoogleLogin
+        clientId={import.meta.env.VITE_APP_GAPI_CLIENT_ID}
+        buttonText='Login'
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={'single_host_origin'}
+        isSignedIn={true}/>
     </div>
   )
 }
 
 export default App
+
+
+{/* <GoogleLogout
+  clientId={import.meta.env.VITE_APP_GAPI_CLIENT_ID}
+  buttonText="Logout"
+  onSuccess={onSuccess}
+/> */}
