ce4f5a6e763dc8160eb86b6b84fa861010efe952
João Mello <joao.mellof@furg.br>
2022-07-10 21:46:02 -0300

    creating custom select

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -1,4 +1,5 @@
-import { FiFilter } from 'react-icons/fi'
+import React, { MutableRefObject, useEffect, useRef, useState } from 'react';
+import { FiFilter, FiChevronDown } from 'react-icons/fi'
 
 import './styles.scss'
 
@@ -8,11 +9,76 @@ interface FiltersProps{
   changeFormMethod: (method: 'POST' | 'PUT') => void;
 }
 
+interface FilterTypeOption{
+  value: string;
+  text: string;
+}
+
+interface CustomClickEvent extends React.MouseEvent<HTMLLIElement, MouseEvent>{
+  target:{
+    addEventListener: () => void;
+    dispatchEvent: () => boolean; 
+    removeEventListener: () => void;
+    id: string;
+    textContent: string;
+  }
+}
+
 export function Filters({ 
   customStyles, 
   onNewRegisterButtonClick,
   changeFormMethod 
 }: FiltersProps){
+  const baseFilterTypeOptions: FilterTypeOption[] = [
+    { value: 'name', text: 'Nome do ativo' },
+    { value: 'date', text: 'Data da compra / venda' },
+    { value: 'value', text: 'Valor' },
+    { value: 'amount', text: 'Quantidade' },
+    { value: 'classify', text: 'Classificação' },
+  ];
+
+  const [filterTypeValue, setFilterTypeValue] = useState('')
+  const [filterTypeOptions, setFilterTypeOptions] = useState<FilterTypeOption[]>(baseFilterTypeOptions)
+  const [actualFilterType, setActualFilterType] = useState('');
+  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+  
+  const filterInputRef = useRef() as MutableRefObject<HTMLInputElement>;
+  const ulDropdownRef = useRef() as MutableRefObject<HTMLUListElement>;
+
+  useEffect(()=>{
+    window.addEventListener('mousedown',(e)=>{
+      const ul = ulDropdownRef.current;
+      setIsDropDownOpen((state)=>{
+        if(!ul?.contains(e.target as Node) && state) return false
+        return state
+      });
+    })
+  },[])
+
+  function handleFilterTypeValueChange(e: React.ChangeEvent<HTMLInputElement>){
+    const newFilterTypeValue = e.target.value
+    setFilterTypeValue(newFilterTypeValue);
+    
+    const filteredTypeOptions = baseFilterTypeOptions
+      .filter((option)=>{
+        console.log(newFilterTypeValue)
+        if(!newFilterTypeValue) return true;
+        return option.text.toLowerCase()
+          .includes(newFilterTypeValue.toLocaleLowerCase())
+    });
+    setFilterTypeOptions(filteredTypeOptions);
+  }
+
+  function handleSelectFilter(e: CustomClickEvent){
+    setActualFilterType(e.target.id);
+    setFilterTypeValue(e.target.textContent);
+    setTimeout(()=>{
+      filterInputRef?.current?.focus()
+      setIsDropDownOpen(false)
+    },100);
+  }
+  
+  
 
   return(
     <div className="filters-container" style={customStyles}>
@@ -22,21 +88,43 @@ export function Filters({
           id="input"
           type="text" 
           placeholder='Digite o filtro ...'
+          ref={filterInputRef}
         />
         <FiFilter/>
       </label>
-      
-      <select 
-        className="select" 
-        name="filter-type" 
-        id="filter-type"
-      >
-        <option value="name">Nome da ação</option>
-        <option value="date">Data da compra/venda</option>
-        <option value="value">Valor</option>
-        <option value="amount">Quantidade</option>
-        <option value="classify">Classificação</option>
-      </select>
+
+      <label htmlFor="select" className="input-container">
+        <input 
+          className="input" 
+          id="select"
+          type="text" 
+          value={filterTypeValue}
+          onChange={handleFilterTypeValueChange}
+          onFocus={()=>{ setIsDropDownOpen(true); }}
+          placeholder='Escolha o filtro'
+        />
+        <FiChevronDown/>
+        <ul 
+          className="input-dropdown" 
+          ref={ulDropdownRef}
+          style={{ visibility: isDropDownOpen ? 'visible': 'hidden', 
+            top: isDropDownOpen ? '2.1rem' : '1rem'
+          }}
+        >
+          { filterTypeOptions.map((option)=>{
+            return(
+              <li 
+                key={option.value} 
+                value={option.value}
+                id={option.value}
+                onClick={handleSelectFilter}
+              >
+                {option.text}
+              </li>
+            )
+          }) }
+        </ul>
+      </label>
 
       <button 
         className="button"
